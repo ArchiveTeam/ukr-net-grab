e86c27f814fc70f1cbdca8534c6564435390b753
arkiver <arkiver@protonmail.com>
2022-03-23 16:15:12 +0100

    Version 20220323.01. Fix backfeed. Fix maxtries use.

--- pipeline.py
+++ pipeline.py
@@ -64,7 +64,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20220304.01'
+VERSION = '20220323.01'
 TRACKER_ID = 'ukr-net'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 40
